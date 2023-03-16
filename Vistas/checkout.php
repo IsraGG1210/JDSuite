7054971a46fa892b400074a6db635109011750a9
IsraGG1210 <isragargal@outlook.com>
2023-03-16 16:37:01 -0600

    ca

--- Vistas/checkout.php
+++ Vistas/checkout.php
@@ -66,12 +66,12 @@ require_once './Conexion/funciones.php';
                 <div class="accordion-item">
                 <h2 class="accordion-header" id="headingThreeY">
                     <button class="accordion-button collapsed" type="button" data-mdb-toggle="collapse" style="color:black"
-                    data-mdb-target="#collapseThreeY" aria-expanded="false" aria-controls="collapseThreeY">
+                    data-mdb-target="#collapseThreep" aria-expanded="false" aria-controls="collapseThreep">
                     <i class="fa-sharp fa-solid fa-list fa-lg me-2 opacity-70"></i>
                     <h3>Transferencias</h3>
                     </button>
                 </h2>
-                <div id="collapseThreeY" class="accordion-collapse collapse" aria-labelledby="headingThreeY"
+                <div id="collapseThreep" class="accordion-collapse collapse" aria-labelledby="headingThreeY"
                     data-mdb-parent="#accordionExampleY">
                     <div class="accordion-body text-justify">
                     <br>
@@ -115,7 +115,85 @@ require_once './Conexion/funciones.php';
             </div>
             </div>
             <div class="col-md-4">
-                segunda
+            <div class=" lip row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <h1>RESUMEN</h1>
+              <div class="sep">
+              </div>
+              <div class="flex justify-between mb-3 text-sm">
+                <div class="row">
+                  <div class="col-9">
+                    <span >Subtotal</span>
+                    <span id="cantcart">(
+                      <?php
+                          
+                          $newitem = 0;
+                          $cantidad_total = 0;
+
+                          if(isset($_SESSION['username'])){ 
+                              $sesion = $_SESSION['username'];
+                              //echo $sesion;
+                              $sql = 'SELECT COUNT(pd_cantidad) AS cantidad FROM pedidoscld WHERE pd_pedido="'.$sesion.'"AND pd_conf = 0';
+                              $result = setq($sql);
+                              $cantidad_tota = mysqli_fetch_assoc($result);
+                              $cantidad_total = $cantidad_tota['cantidad'];
+                          //echo $cantidad_total;
+                          
+
+                          }else{
+                              if(isset($_COOKIE['cart'])) {
+                              
+                                  foreach($_COOKIE['cart'] as $clave=>$item) {
+                                      $cantidad_total += $item[1];
+                                  }
+                                  //echo $cantidad_total;
+                              }
+                          } 
+                          echo $cantidad_total;
+                          ?>)
+                    </span>
+                    <span >productos</span>
+                  </div>
+                  <div class="col-3">
+                    <h5><?php  echo MONEDA. number_format($total,2,'.',',');?></h5>
+                  </div>
+                </div>
+                <div class="flex justify-between mb-3 text-sm">
+                <div class="row">
+                  <div class="col-9">
+                    <span >Envio</span>
+                  </div>
+                  <div class="col-3">
+                    <h5>
+                      <?php 
+                      $envio = 0;
+                      if($total>5000){
+                        echo MONEDA.$envio;
+                      } elseif($total==0){
+                        echo MONEDA.$envio;
+                      }else{
+                        $envio = 150;
+                        echo MONEDA.$envio;
+                      }
+                      ?>
+                    </h5>
+                  </div>
+                </div>
+                <div class="flex justify-between font-bold pt-4  border-t border-gray-500"id="totaldespues">
+                <div class="row">
+                  <div class="col-9">
+                    <span ><h4 id="totalf"><b>Total a pagar por el momento</b></h4></span>
+                  </div>
+                  <div class="col-3">
+                  <?php $totalen = $total+$envio;?>
+                    <span>
+                      <h5 id="idtotalFinal" data-total=""><b>
+                      <?php 
+                      echo MONEDA. number_format($totalen,2,'.',',');?>
+                    </b></h5></span>
+                  </div>
+                </div>
+          </div>
+        </div>
             </div>
         </div>
     </div>
