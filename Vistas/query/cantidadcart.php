a9664dffbc1d6c1061cde2c4557e099994a35168
IsraGG1210 <isragargal@outlook.com>
2023-03-21 16:35:32 -0600

    Cambio

--- Vistas/query/cantidadcart.php
+++ Vistas/query/cantidadcart.php
@@ -2,12 +2,17 @@
 require_once '../Conexion/funciones.php';
 session_start();
 $newitem = 0;
-$cantidad_total = 7;
+$cantidad_total = 0;
 
 if(isset($_SESSION['username'])){ 
     $sesion = $_SESSION['username'];
+    $sql1 ="SELECT c_id FROM clientes WHERE c_mail ='$sesion'";
+$resultado = setq($sql1);
+$idusuario = mysqli_fetch_array($resultado);
+$idusu = $idusuario['c_id'];
     //echo $sesion;
-    $sql = 'SELECT COUNT(pd_cantidad) AS cantidad FROM pedidoscld WHERE pd_pedido="'.$sesion.'" AND pd_conf = 0';
+   /*  $sql = 'SELECT COUNT(pd_cantidad) AS cantidad FROM pedidoscld WHERE pd_pedido="'.$sesion.'" AND pd_conf = 0'; */
+    $sql = "SELECT COUNT(pd_cantidad) AS cantidad FROM pedidoscld WHERE pd_pedido='$idusu' AND pd_conf = 0";
     $result = setq($sql);
     $cantidad_tota = mysqli_fetch_assoc($result);
     $cantidad_total = $cantidad_tota['cantidad'];
